507e9e03cdb8d1975cef74e4b6a015b735a80bb2
rubbybbs <rubbybbs1324@gmail.com>
2018-05-28 00:05:03 +0800

    debug + 试运行  问题：析构函数之前写法释放指针会出错，重复释放了

--- game.cpp
+++ game.cpp
@@ -1,8 +1,53 @@
 //主游戏文件
 #include <iostream>
-#include <goGameBase.h>
-#include <difGame.h>
+#include "goGameBase.h"
+#include "difGame.h"
+
+void PrintChessBoard(goGameBase type);
+
 int main(){
   goGameBase FIR;
-  
+  while(1)
+  {
+      int x, y;
+      cin >> x >> y;
+      FIR.changeMatrix(x, y);
+      PrintChessBoard(FIR);
+      FIR.stepPlus();
+  }
 }
+
+
+void PrintChessBoard(goGameBase type)    //打印棋盘，这个函数可以自己调整
+    {
+        system("cls");                //系统调用，清空屏幕
+        for (int i = 0; i < type.getsize()+1; ++i)
+        {
+            for (int j = 0; j < type.getsize()+1; ++j)
+            {
+                if (i == 0)                               //打印列数字
+                {
+                    if (j!=0)
+                        printf("%d  ", j);
+                    else
+                        printf("   ");
+                }
+                else if (j == 0)                //打印行数字
+                    printf("%2d ", i);
+                else
+                {
+                    if (i < type.getsize()+1)
+                    {
+                        printf("%c |",type.getMatrix(i, j));
+                    }
+                }
+            }
+            cout << endl;
+            cout << "   ";
+            for (int m = 0; m < type.getsize(); m++)
+            {
+                printf("--|");
+            }
+            cout << endl;
+        }
+    }
--- goGameBase.h
+++ goGameBase.h
@@ -1,39 +1,73 @@
 //游戏基类头文件
 #include <iostream>
 #include <ctime>
-#include <stdlib>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
 class goGameBase{
 private:
 int size;
-int** Marix;// 矩阵状态，支持未知大小
+int** Matrix;// 矩阵状态，支持未知大小
 int step;// 下过的步数
 bool firstPlayer;
-vector <int> x;
-vector <int> y;
-public：
-goGameBase(): size(19), step(0),firstPlayer(srand((unsigned)time(nullptr)%2)) {
-    Marix = new int*[size];
+vector <int> xPath;
+vector <int> yPath;
+public:
+goGameBase(): size(19), step(0){
+    
+    srand((unsigned)time(nullptr));//生成随机数种子
+    firstPlayer = rand() % 2;//将先手赋随机初值
+    Matrix = new int*[size];
     for(int i = 0; i < size;i ++)
     {
-        Marix[i]= new int[size](0);//如果不行把初始化分开写
+        Matrix[i]= new int[size];//如果不行把初始化分开写  （好像确实不行。。。)
+        for(int j = 0;j < size;j++)
+        {
+            Matrix[i][j] = -1;
+        }
     }
-}；
-goGameBase(int mySize): size(mySize), step(0),firstPlayer(srand((unsigned)time(nullptr)%2)) {
-    Marix = new int*[size];
+};
+goGameBase(int mySize): size(mySize), step(0){
+    
+    srand((unsigned)time(nullptr));//生成随机数种子
+    firstPlayer = rand() % 2;//将先手赋随机初值
+    Matrix = new int*[size];
     for(int i = 0; i < size;i ++)
     {
-        Marix[i]= new int[size](0);//如果不行把初始化分开写
+        Matrix[i]= new int[size];//如果不行把初始化分开写    (好像确实不行。。。)
+        for(int j = 0;j < size;j++)
+        {
+            Matrix[i][j] = -1;
+        }
     }
+}
 // virtual void changeMarix(int x, int y)=0;
 // virtual bool judge(int x, int y)=0;
 // virtual bool isEnd()=0;
 // for test----------------------------
-int getsize(){return size;}; 
-void changeMarix(int x, int y);
+int getsize(){return size;};
+char getMatrix(int x, int y)
+{
+    if(Matrix[x][y] == -1)
+        return ' ';
+    else if(Matrix[x][y] == 0)
+        return 'o';
+    else if(Matrix[x][y] == 1)
+        return 'x';
+};//绘制矩阵时将数字转换为符号
+void changeMatrix(int x, int y)
+{
+    Matrix[x][y] = (step + firstPlayer) % 2 + 1;
+    xPath.push_back(x);
+    yPath.push_back(y);
+}; //每一次落子后改变矩阵的状态 （同时记录当前步骤？？）
+void stepPlus(){ step++; }
 bool judge(int x, int y);
 bool isEnd();
 //-------------------------------------
 virtual ~goGameBase(){
-    delete[] Marix;//防止内存泄漏
+    delete[] Matrix;//防止内存泄漏
 }
 };
