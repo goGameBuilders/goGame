0250ff9afcc79342adfdfa23d05f02fb98dae2ae
skipper17 <ssk52839916@gmail.com>
2018-05-28 12:23:33 +0800

    ssk调整了结构，写了regret

--- game.cpp
+++ game.cpp
@@ -2,65 +2,88 @@
 #include <iostream>
 #include <stdlib.h>
 #include "goGameBase.h"
-#include "difGame.h"
+//#include "difGame.h"
 
-void PrintChessBoard(goGameBase type);
+void PrintChessBoard( goGameBase &type);
 
-int main(){
-  goGameBase FIR;
-    PrintChessBoard(FIR);
-  while(1)
-  {
-      int x, y;
-      while(1)
-    {
-        cin >> x >> y;
-        if(FIR.judge(x, y))
-            break;
-        else
-        {
-            cout<<"这个地方已经有棋子了，请重新输入坐标："<<endl;
-            continue;
-        }
-    }
-      FIR.changeMatrix(x, y);
-      PrintChessBoard(FIR);
-      FIR.stepPlus();
-  }
+int main() {
+	goGameBase FIR(20);
+	PrintChessBoard(FIR);
+	while (1)
+	{
+		
+		int x, y, choice;
+		while (1) {
+			cout << "1代表下子，2代表悔棋,请输入：" << endl;
+			cin >> choice;
+			if (choice == 1)
+			{
+				while (1)
+				{
+					cin >> x >> y;
+					if (FIR.judge(x, y))
+						break;
+					else
+					{
+						cout << "这个地方已经有棋子或者越界了，请重新输入坐标：" << endl;
+						continue;
+					}
+				}
+				break;
+			}
+			else if (choice == 2)
+			{
+				if (FIR.regret())
+				{
+					PrintChessBoard(FIR);
+					cout << "regret successfully" << endl;
+				}
+				else
+					cout << "you can't regret" << endl;
+			}
+		}
+		FIR.changeMatrix(x, y);
+		PrintChessBoard(FIR);
+	}
 }
 
 
-void PrintChessBoard(goGameBase type)    //打印棋盘，这个函数可以自己调整
-    {
-        system("clear");                //系统调用，清空屏幕
-        //system("cls")     在mac上这句报错。。应该是win上用的？
-        for (int i = 0; i < type.getsize()+1; ++i)
-        {
-            for (int j = 0; j < type.getsize()+1; ++j)
-            {
-                if (i == 0)                               //打印列数字
-                {
-                    if (j!=0)
-                        printf("%d  ", j);
-                    else
-                        printf("   ");
-                }
-                else if (j == 0)                //打印行数字
-                    printf("%2d ", i);
-                else
-                {
-                    if (i < type.getsize()+1)
-                    {
-                        printf("%c |",type.getMatrix(i, j));
-                    }
-                }
-            }
-            cout << endl;
-            cout << "   ";
-            for (int m = 0; m < type.getsize(); m++)
-            {
-                printf("--|");
-            }
-            cout << endl;
-        }
-    }
+void PrintChessBoard( goGameBase& type)    //打印棋盘，这个函数可以自己调整
+{
+	system("cls");                //系统调用，清空屏幕
+									//system("cls")     在mac上这句报错。。应该是win上用的？
+	if (type.getWhoTurn()) {
+		cout << "Black's Turn" << endl;
+	}
+	else
+		cout << "White's Trun" << endl;
+	for (int i = 0; i < type.getsize() + 1; ++i)
+	{
+		for (int j = 0; j < type.getsize() + 1; ++j)
+		{
+			if (i == 0)                               //打印行数字
+			{
+				if (j != 0)
+					printf(" %2d ", j);
+				else
+					printf("    ");
+			}
+			else if (j == 0)                //打印列数字
+				printf(" %2d ", i);
+			else
+			{
+				if (i < type.getsize() + 1)
+				{
+					printf(" %c |", type.getMatrix(i, j));
+				}
+			}
+		}
+		cout << endl;
+		cout << "    ";
+		for (int m = 0; m < type.getsize(); m++)
+		{
+			printf("---|");
+		}
+		cout << endl;
+	}
+}
--- goGameBase.h
+++ goGameBase.h
@@ -1,3 +1,4 @@
+#pragma once
 //游戏基类头文件
 #include <iostream>
 #include <ctime>
@@ -6,74 +7,106 @@
 
 using namespace std;
 
-class goGameBase{
+class goGameBase {
 private:
-int size;
-int** Matrix;// 矩阵状态，支持未知大小
-int step;// 下过的步数
-bool firstPlayer;
-vector <int> xPath;
-vector <int> yPath;
+	int size;
+	int** Matrix;// 矩阵状态，支持未知大小
+	int step;// 下过的步数
+	bool firstPlayer;
+	vector <int> xPath;
+	vector <int> yPath;
 public:
-goGameBase(): size(19), step(0){
-    
-    srand((unsigned)time(nullptr));//生成随机数种子
-    firstPlayer = rand() % 2;//将先手赋随机初值
-    Matrix = new int*[size + 1];
-    for(int i = 0; i <= size;i ++)
-    {
-        Matrix[i]= new int[size + 1];//如果不行把初始化分开写  （好像确实不行。。。)
-        for(int j = 0;j <= size;j++)
-        {
-            Matrix[i][j] = -1;
-        }
-    }
-};
-goGameBase(int mySize): size(mySize), step(0){
-    
-    srand((unsigned)time(nullptr));//生成随机数种子
-    firstPlayer = rand() % 2;//将先手赋随机初值
-    Matrix = new int*[size + 1];
-    for(int i = 0; i <= size;i ++)
-    {
-        Matrix[i]= new int[size + 1];//如果不行把初始化分开写    (好像确实不行。。。)
-        for(int j = 0;j <= size;j++)
-        {
-            Matrix[i][j] = -1;
-        }
-    }
-}
-// virtual void changeMarix(int x, int y)=0;
-// virtual bool judge(int x, int y)=0;
-// virtual bool isEnd()=0;
-// for test----------------------------
-int getsize(){return size;};
-char getMatrix(int x, int y)
-{
-    if(Matrix[x][y] == -1)
-        return ' ';
-    else if(Matrix[x][y] == 0)
-        return 'o';
-    else if(Matrix[x][y] == 1)
-        return 'x';
-};//绘制矩阵时将数字转换为符号
-void changeMatrix(int x, int y)
-{
-    Matrix[x][y] = (step + firstPlayer) % 2;
-    xPath.push_back(x);
-    yPath.push_back(y);
-}; //每一次落子后改变矩阵的状态 （同时记录当前步骤？？）
-void stepPlus(){ step++; }
-bool judge(int x, int y)
-{
-    if (Matrix[x][y] == -1)
-        return true;
-    else
-        return false; 
-};
-bool isEnd();
-//-------------------------------------
-virtual ~goGameBase(){
-   
-}
+	goGameBase() : size(19), step(0) {
+
+		srand((unsigned)time(nullptr));//生成随机数种子
+		firstPlayer = rand() % 2;//将先手赋随机初值
+		Matrix = new int*[size + 1];
+		for (int i = 0; i <= size; i++)
+		{
+			Matrix[i] = new int[size + 1];//如果不行把初始化分开写  （好像确实不行。。。)
+			for (int j = 0; j <= size; j++)
+			{
+				Matrix[i][j] = -1;
+			}
+		}
+	};
+	goGameBase(int mySize) : size(mySize), step(0) {
+
+		srand((unsigned)time(nullptr));//生成随机数种子
+		firstPlayer = rand() % 2;//将先手赋随机初值
+		Matrix = new int*[size + 1];
+		for (int i = 0; i <= size; i++)
+		{
+			Matrix[i] = new int[size + 1];//如果不行把初始化分开写    (好像确实不行。。。)
+			for (int j = 0; j <= size; j++)
+			{
+				Matrix[i][j] = -1;
+			}
+		}
+	}
+	void changeMatrix(int x, int y)
+	{
+		xPath.push_back(x);
+		yPath.push_back(y);
+		step++;
+		updateMarix(x, y);
+	}; //每一次落子后改变矩阵的状态 （同时记录当前步骤？？）
+	  // 把update分开来，记录是通用的
+	bool regret() {
+		if (step >= 2)
+		{
+			step -= 2;
+			xPath.pop_back();
+			xPath.pop_back();
+			yPath.pop_back();
+			yPath.pop_back();
+			updateMarixTotal();
+			return true;
+		}
+		else
+			return false;
+	}
+	// virtual bool judge(int x, int y)=0;
+	// virtual bool isEnd()=0;
+	// for test----------------------------
+	void updateMarixTotal(){ 
+		for (int i = 1; i <= size; i++)
+			for (int j = 1; j <= size; j++)
+				Matrix[i][j] = -1;
+		for (int i = 0; i < step; i++)
+		{
+			updateMarix(xPath[i], yPath[i]);
+		}
+	};
+	void updateMarix(int x, int y) {//最后应当是虚函数
+		Matrix[x][y] = (step + firstPlayer) % 2;
+	};
+	const int getsize() { return size; };
+	const bool getWhoTurn() { return (firstPlayer + step) % 2; };
+	char getMatrix(int x, int y)
+	{
+		if (Matrix[x][y] == -1)
+			return ' ';
+		else if (Matrix[x][y] == 0)
+			return 'o';
+		else if (Matrix[x][y] == 1)
+			return 'x';
+	};//绘制矩阵时将数字转换为符号
+
+
+	bool judge(int x, int y)
+	{
+		if (x > 0 && x <= size && y > 0 && y <= size && Matrix[x][y] == -1)
+		return true;
+		else
+			return false;
+	};
+	bool isEnd();
+	//-------------------------------------
+	virtual ~goGameBase() {
+		for (int i = 0; i <= size; i++) {
+			delete[] Matrix[i];
+		}
+		delete[] Matrix;
+	}
 };
