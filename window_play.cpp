07d3465c304f4aac2fdf281b68a05315c3c6515c
rubbybbs <rubbybbs1324@gmail.com>
2018-06-14 20:51:26 +0800

    增加了一些效果

--- window_play.cpp
+++ window_play.cpp
@@ -17,6 +17,9 @@ Window_Play::Window_Play(goGamePlatform* _gameplatform,QWidget *parent) :
     gameplatform(_gameplatform)
 {
     ui->setupUi(this);
+    ui->centralwidget->setMouseTracking(true);
+    ui->label->setMouseTracking(true);
+    setMouseTracking(true);
 }
 
 Window_Play::~Window_Play()
@@ -28,6 +31,8 @@ void Window_Play::paintEvent(QPaintEvent *)
 {
   paint=new QPainter(this);
   paint->begin(this);//主窗口
+  this->setMouseTracking(true);
+
 
  if(!msgFirst)
  {
@@ -50,25 +55,40 @@ void Window_Play::paintEvent(QPaintEvent *)
   QBrush brush;
   brush.setStyle(Qt::SolidPattern);
 
+  // 绘制落子标记
+     if (nowPosCol > 0 && nowPosRow <= SIZE &&
+         nowPosRow > 0 && nowPosCol <= SIZE &&
+         gameplatform->getgame()->getMatrix(nowPosRow, nowPosCol) == -1)
+     {
+         if (gameplatform->getgame()->getWhoTurn())
+             brush.setColor(QColor(0, 0, 0, 150));
+         else
+             brush.setColor(QColor(255, 255, 255, 150));
+
+         paint->setBrush(brush);
+
+         if(isPointed)
+            paint->drawEllipse(margin_x + nowPosCol * block_size - block_size/4,margin_y + nowPosRow * block_size - block_size/4,block_size/2,block_size/2);
+         else
+            paint->drawEllipse(nowPosx_accurate - block_size/4,nowPosy_accurate - block_size/4,block_size/2,block_size/2);
+     }
+
+
+
   for(int i = 1; i <=SIZE; i ++)
     for(int j = 1; j <=SIZE; j ++)
     {
         if(gameplatform->getgame()->getMatrix(i, j)==0){
-            brush.setColor(Qt::black);
+            brush.setColor(QColor(0, 0, 0, 255));
             paint->setBrush(brush);
             paint->drawEllipse(margin_x + j * block_size - block_size/4,margin_y + i * block_size - block_size/4,block_size/2,block_size/2); //画椭圆：中心点X,Y,宽度，高度
         }
         else if(gameplatform->getgame()->getMatrix(i, j)==1){
-            brush.setColor(Qt::white);
+            brush.setColor(QColor(255, 255, 255, 255));
             paint->setBrush(brush);
             paint->drawEllipse(margin_x + j * block_size - block_size/4,margin_y + i * block_size - block_size/4,block_size/2,block_size/2); //画椭圆：中心点X,Y,宽度，高度
         }
     }
-
-//  //在棋盘正中做一个黑棋子
-//  paint->setBrush(QBrush(Qt::black,Qt::SolidPattern));//毛刷：颜色，实图案
-//  paint->drawEllipse((SIZE+0.5)*WIDTH/2,(SIZE+0.5)*WIDTH/2,WIDTH/2,WIDTH/2);//画椭圆：中心点X,Y,宽度，高度
-//  paint->end();                   //a model for making chesspiece
 
   paint->end();
 
@@ -142,7 +162,71 @@ void Window_Play::mousePressEvent(QMouseEvent *event)
     QString gameinfo = gameplatform->getgame()->getinfo().c_str();
     ui->label->setText(gameinfo);
     update();
-}               //鼠标事件函数
+}               //鼠标点击事件函数
+
+
+void Window_Play::mouseMoveEvent(QMouseEvent *event)
+{
+    int tmpx = event->pos().x();
+    int tmpy = event->pos().y();
+    nowPosx_accurate = event->pos().x();
+    nowPosy_accurate = event->pos().y();
+    isPointed = false;
+    if(tmpx >= margin_x + block_size / 2 && tmpx < margin_x + (block_size) * (SIZE - 1/2)
+    && tmpy >= margin_y + block_size / 2 && tmpy < margin_y + (block_size) * (SIZE - 1/2) )        //棋盘边界判断
+    {
+        int col = (tmpx - margin_x) / block_size;
+        int row = (tmpy - margin_y) / block_size;        //获取离点击点最近的棋盘点坐标(左上角)
+
+        int leftTopx = margin_x + block_size * col;
+        int leftTopy = margin_y + block_size * row; //获取左上角真实坐标
+
+
+        int len = 0;        //误差值
+
+
+        len = sqrt((tmpx - leftTopx) * (tmpx - leftTopx) + (tmpy - leftTopy) * (tmpy - leftTopy));
+               if (len < mouse_delta)
+               {
+                   nowPosRow = row;
+                   nowPosCol = col;
+                   isPointed = true;
+               }
+               len = sqrt((tmpx - leftTopx - block_size) * (tmpx - leftTopx - block_size) + (tmpy - leftTopy) * (tmpy - leftTopy));
+               if (len < mouse_delta)
+               {
+                   nowPosRow = row;
+                   nowPosCol = col + 1;
+                   isPointed = true;
+               }
+               len = sqrt((tmpx - leftTopx) * (tmpx - leftTopx) + (tmpy - leftTopy - block_size) * (tmpy - leftTopy - block_size));
+               if (len < mouse_delta)
+               {
+                   nowPosRow = row + 1;
+                   nowPosCol = col;
+                   isPointed = true;
+               }
+               len = sqrt((tmpx - leftTopx - block_size) * (tmpx - leftTopx - block_size) + (tmpy - leftTopy - block_size) * (tmpy - leftTopy - block_size));
+               if (len < mouse_delta)
+               {
+                   nowPosRow = row + 1;
+                   nowPosCol = col + 1;
+                   isPointed = true;
+               }
+
+    }   //if
+
+    else
+    {
+        nowPosRow = -1;
+        nowPosCol = -1;
+    }
+
+     update();
+
+}
+
+
 
 
 
--- window_play.h
+++ window_play.h
@@ -16,7 +16,8 @@ public:
     ~Window_Play();
 
     void paintEvent(QPaintEvent *);         //绘图函数声明
-    void mousePressEvent(QMouseEvent *event);    //监视鼠标移动情况函数
+    void mousePressEvent(QMouseEvent *event);    //监视鼠标点击情况函数
+    void mouseMoveEvent(QMouseEvent *event);    //监视鼠标移动状况函数
 
 
 private slots:
@@ -42,9 +43,13 @@ private:
     const int mouse_delta = 10;    //判定有效点击的最小误差
     int ClickPosRow = -1;          //鼠标点击的行数
     int ClickPosCol = -1;          //鼠标点击的列数
+    int nowPosRow = -1;            //目前鼠标所在行数
+    int nowPosCol = -1;            //目前鼠标所在列数
+    int nowPosx_accurate = -1;  //目前鼠标所在位置x(精确)
+    int nowPosy_accurate = -1;  //目前鼠标所在位置y(精确)
 
-    bool msgFirst = false;
-
+    bool msgFirst = false;         //是否已经显示起始信息
+    bool isPointed = false;        //鼠标是否在格点上
     QPainter *paint;
 };
 
